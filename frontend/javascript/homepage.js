d1bf04b09975c2ee53b314509ffbdc18943d3a48
vPt-King <vpthanh1302@gmail.com>
2024-02-17 11:15:45 +0700

    sort by view day all page

--- frontend/javascript/homepage.js
+++ frontend/javascript/homepage.js
@@ -21,3 +21,16 @@ function sortAll(event)
     let paginateArea = document.getElementById("paginate_show");
     showMangasInSpecificPage(mangas, mangaArea, paginateArea, 1);
 }
+
+function sortByViewDay(event){
+    let showOption = document.getElementById("show_option");
+    showOption.innerHTML = "Xem nhiều nhất trong ngày";
+    let mangas = JSON.parse(localStorage.getItem("mangas"));
+    mangas.sort((a, b) => {
+        return b.view_day - a.view_day;
+    });
+    localStorage.setItem("mangas", JSON.stringify(mangas));
+    let mangaArea = document.getElementById("list_manga_detail");
+    let paginateArea = document.getElementById("paginate_show");
+    showMangasInSpecificPage(mangas, mangaArea, paginateArea, 1);
+}
