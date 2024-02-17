50c68ebeb5b692ac10edbf81c9f49bf4376c38f8
vPt-King <vpthanh1302@gmail.com>
2024-02-17 15:22:56 +0700

    add function sort by month for all file

--- frontend/javascript/listMangaOfAuthor.js
+++ frontend/javascript/listMangaOfAuthor.js
@@ -52,3 +52,16 @@ function sortByViewWeek(event){
     let paginateArea = document.getElementById("paginate_show");
     showMangasInSpecificPage(mangas, mangaArea, paginateArea, 1);
 }
+
+function sortByViewMonth(event){
+    let showOption = document.getElementById("show_option");
+    showOption.innerHTML = "Xem nhiều nhất trong tháng";
+    let mangas = JSON.parse(localStorage.getItem("mangas"));
+    mangas.sort((a, b) => {
+        return b.view_month - a.view_month;
+    });
+    localStorage.setItem("mangas", JSON.stringify(mangas));
+    let mangaArea = document.getElementById("list_manga_detail");
+    let paginateArea = document.getElementById("paginate_show");
+    showMangasInSpecificPage(mangas, mangaArea, paginateArea, 1);
+}
